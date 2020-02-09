b70285c8473e0e21434384f8a1c240541362209d
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-09 17:22:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,10 @@ label: "Kingdom",
 sub_category: {
     label: "Phylum",
     sub_category: {
-      label: "Class"
+      label: "Class",
+      sub_category: {
+        label: "Order"
+      }
     }
   }
 }
