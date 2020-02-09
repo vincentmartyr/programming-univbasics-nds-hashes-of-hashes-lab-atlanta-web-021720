1e5462d7e8595c69fd2cda16d4e80a1920e7efa3
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-09 17:25:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,7 +49,19 @@ sub_category: {
     sub_category: {
       label: "Class",
       sub_category: {
-        label: "Order"
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: {
+                label: nil
+              }
+            }
+          }
+        }
       }
     }
   }
